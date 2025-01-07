6fd02ea39782b7d4eccda31afcabb44ff1e184bb
jre98 <jre98@csu.fullerton.edu>
2025-01-07 02:04:20 -0600

    Calling helper funcs. in main file and creating file to get list of Gen II moves

--- Gen II/extract_move.py
+++ Gen II/extract_move.py
@@ -2,137 +2,20 @@ import requests # type: ignore
 from bs4 import BeautifulSoup # type: ignore
 import re
 
-# Helper functions that process each possible way a Pokemon can learn a move (via:
-# Level Up, TM/HM, Breeding/Egg Moves, and Special Events)
+# Import helper functions
+from extract_helper_funcs import *
 
-# Pulls the data about which Pokemon can learn the move via Level Up and returns a 
-# list of the names of those Pokemon
-def get_byleveup(levelup_section):
-    # Find the table right after the "level" anchor
-    table = levelup_section.find_next("table", class_="dextable")
+# Import function that will get the list of all moves from Gen II
+from get_gen2_movelist import get_moves
 
-    # Pull the data from each row in the table except the first two. The first row
-    # contains header info such as Pokedex #, Pokemon Name, Base Stats, level the 
-    # Pokemon learns the move at, etc. while the second row contains subheadings 
-    # for the Base Stats (HP, Attack, etc.) and for the level the Pokemon learns 
-    # the move at (Gold/Silver vs. Crystal) 
-    levelup_rows = table.find_all("tr")[2:] 
-  
-    # Container to store the results
-    levelup_data = []
 
-    # Examine each individual row from the list of rows
-    for row in levelup_rows:
-        # print("The current row in LEVEUP is:\n", row, "\n")
-        # Get the data for each column in the current row
-        cols = row.find_all("td", class_="fooinfo")
-        # Make sure there are enough columns so that the correct data is extacted
-        if len(cols) > 11:
-            # Need to extract the 3rd column for the Pokemon's name and also the 11th 
-            # and 12th columns for the Levels the move is leared at in G/S vs. C  
-            pokemon_name = cols[2].get_text(strip=True)
 
-            # Level move is learned at in Gold/Silver
-            level_GS = cols[10].get_text(strip=True)
-            # Level move is learned at in Crystal
-            level_C = cols[11].get_text(strip=True)
+# Dictionary containing all moves from the Gen II Pokemon Games
+all_moves = {}
 
-            # If multiple levels are listed, split them and reformat (correct split location found
-            # using a regular expression)
-            level_GS_formatted = ", ".join(re.findall(r'Lv\.\s?\d+', level_GS))
-            # level_C_formatted = ", ".join([f"{level}" for level in level_C.split(", ")])
-            level_C_formatted = ", ".join(re.findall(r'Lv\.\s?\d+', level_C))
+# Call helper function to populate dictionary
+all_moves = get_moves()
 
-            # Format the final data result for the current Pokemon
-            formatted_entry = f"{pokemon_name}: {level_GS_formatted} | {level_C_formatted}"
-
-            # Add the relevant info to the results array
-            levelup_data.append(formatted_entry)
-
-    # Return the data that was extracted
-    return levelup_data
-
-# Pulls the data about which Pokemon can learn the move via TM/HM and returns a 
-# list of the names of those Pokemon (this works for HMs as well as TMs due to the)
-# way the anchor tags are labeled in the HTML
-def get_byTM(TM_section):
-    # Pull the data from each row in the table (skip the first two header rows)
-    TM_rows = TM_section.find_all("tr")[2:]
-
-    # Container to store results
-    TM_data = []
-
-    # Examine each individual row from the list of rows
-    for row in TM_rows:
-        # Get the data for each column in the current row
-        cols = row.find_all("td", class_="fooinfo")
-
-        # Skips rows that contain undeeded data (only need Mon names)
-        if len(cols) > 9:
-            # Extract the 3rd column containing the Pokemon's name
-            pokemon_name = cols[2].get_text(strip=True)
-
-            # print("The current Pokemon is: ", pokemon_name)
-
-            # Add the name to the results array
-            TM_data.append(pokemon_name)
-    
-    # Return the extracted data
-    return TM_data
-
-# Pulls the data about which Pokemon can learn the move through Breeding (Egg Moves)
-def get_bybreeding(eggmove_section):
-    # Find the table right after the "egg" anchor
-    table = eggmove_section.find_next("table", class_="dextable")
-
-     # Pull the data from each row in the table (skip the first two header rows)
-    eggmove_rows = table.find_all("tr")[2:]
-
-    # Container to store results
-    eggmove_data = []
-
-    # Examine each row from the list of rows
-    for row in eggmove_rows:
-        # Get the data for each column in the current row
-        cols = row.find_all("td", class_="fooinfo")
-        
-        # Skip rows that don't contain valid data (e.g., rows with Pokémon names)
-        if len(cols) > 9:  
-            # Extract the 3rd column for the Pokémon's name
-            pokemon_name = cols[2].get_text(strip=True)
-
-            # Print the current Pokémon name (optional, for debugging purposes)
-            # print("The current Pokemon is: ", pokemon_name)
-
-            # Add the Pokémon name to the results array
-            eggmove_data.append(pokemon_name)    
-  
-    # Return the extracted data
-    return eggmove_data
-
-# Pulls the data about which Pokemon can learn the move through a Special Event
-def get_byspecialevent(event_section):
-    # Pull the data from each row in the table (skip first two header rows)
-    event_rows = event_section.find_all("tr")[2:]
-
-    # Container to store results
-    event_data = []
-
-    # Examine each individual row from the list of rows
-    for row in event_rows:
-        # Get the data for each column in the current row
-        cols = row.find_all("td", class_="fooinfo")
-
-         # Skips rows that contain undeeded data (only need Mon names)
-        if len(cols) > 9:
-            # Extract the 3rd column containing the Pokemon's name
-            pokemon_name = cols[2].get_text(strip=True)
-
-            # Add the name to the results array
-            event_data.append(pokemon_name)
-
-    # Return the extracted data
-    return event_data
 
 
 
---
+++ Gen II/get_gen2_movelist.py
@@ -0,0 +1,54 @@
+# This file is used to get a list of each possible Gen II move. The list can then be
+# used to loop through each move from Gen II to extract the data about it
+
+import requests # type: ignore
+from bs4 import BeautifulSoup # type: ignore
+
+# Store in a function so it can be called from the main file
+def get_moves():
+    # Choosing the webpage of a random attack, as each attack webpage contains the same
+    # list of moves in a <SELECT> div, which we will extract from
+    curr_url = "https://www.serebii.net/attackdex-gs/ember.shtml"
+
+    # Fetch the page, then parse HTML into a variable
+    response = requests.get(curr_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the <select> tags (the dropdown lists containing each move from Gen II are 
+    # <SELECT> divs with the name "SelectURL"). Since there are three of these, we 
+    # need to store them in a list to deal with each one individually
+    select_tags = soup.find_all("select", {"name": "SelectURL"})
+
+    # Dictionary that will store each move (using a dictionary becuase two-word moves 
+    # are formatted without spaces in the url - "Fire Blast" is "fireblast.shtml"). 
+    # Used to put the correct text in the URL and also in the text document the data is
+    # saved to. Key is unformatted text, value is formatted text.
+    move_dict = {}
+
+    # Loop through each <SELECT> tag, as the lists are stored in three different
+    # dropdown lists.
+    for tag in select_tags:
+        # For the current select tag, iterate through all <option> tags it contains
+        for option in tag.find_all("option"):
+            # Get the value attribute for the current option
+            value = option.get("value")
+            # Make sure value contains data before accessing it
+            if value:
+                # Extract the unformatted name (i.e., "fireblast") from the option. This
+                # would be the text after the second '/' and before '.shtml'
+                key = value.split("/")[-1].split(".")[0]
+
+                # Next, extract the text that is displayed in the dropdown (this is the
+                # correctly ormatted name, i.e., "Fire Blast")
+                display_text = option.get_text(strip=True)
+
+                # Store in dictionary, with unformatted as key and formatted as value
+                move_dict[key] = display_text
+
+    # Now that dictionary is populated, return it
+    return move_dict
+
+
+
+
+
